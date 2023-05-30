2dbc92ae47db8e50e221e84b8575727dcd5fe4fb
Gabor Pihaj <gabor.pihaj@gmail.com>
2023-05-30 22:23:27 +0100

    build: Upgrade nix-rust-utils + use cocogitto

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   description = "Nix cache copy";
 
-  inputs.nru.url = "github:voidcontext/nix-rust-utils";
+  inputs.nru.url = "github:voidcontext/nix-rust-utils?ref=refs/tags/v0.6.0";
 
   outputs = {nru, ...}:
-    nru.lib.mkOutputs ({...}: {
-      src = ./.;
+    nru.lib.mkOutputs ({pkgsUnstable, ...}: {
+      crate = { src = ./.; };
+      buildInputs = [pkgsUnstable.cocogitto];
     });
 }
